0a2ec092f5bbcc27d24e5b565810e8d3bd151625
codeteo <ramones.teo@gmail.com>
2018-04-20 13:58:18 +0000

    Modifies Postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+	public function index() {
+		return Post::all();
+	}
+
+	public function show(Post $post) {
+		return $post;
+	}
+
+	public function store(Request $req) {
+		$post = Post::create($req->all());
+
+		return response()->json($post, 201);
+	}
+
+	public function update(Request $req, Post $post) {
+		$post->update($req->all(();
+
+		return response()->json($post, 200);
+	}
+
+	public function delete(Post $post) {
+		$post->delete();
+
+		return response()->json(null, 204);
+	}
+
 }
